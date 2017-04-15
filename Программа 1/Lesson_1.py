f9a4ac4718deed4b64733db2fae67b8f36ecee52
Lomovskoy <lomovskoy.kirill@yandex.ru>
2017-04-15 12:31:11 +0300

    Программа разбира на функции

--- Программа 1/Lesson_1.py
+++ Программа 1/Lesson_1.py
@@ -1,24 +1,51 @@
 # coding: utf-8
-
-# Python. Быстрый старт.
-# Занятие 3.
-
-# Домашнее задание: 
-#   функциями dir и help исследовать модули os, sys, psutil
-#   вывести максимум информации о системе (ветка №2):
-#   имя текущей директории, платформа (ОС), кодировка файловой системы,
-#   логин пользователя, количество CPU
-
-
 # Подключение модуля производится командой import.
 # Модули os,sys идут в комплекте с Python'ом.
-import os
-import sys
-import shutil
+# Функция type выводит информацию о типе переменной (объекта)
+# Функция dir показывает внутреннее содержимое переменной (объекта) - атрибуты, методы
+# Функция help выводит встронную справку об объекте
+import os, sys, shutil
 # psutil - сторонний модуль, нужно установить через pip install psutil
-import psutil       
-
-
+import psutil
+#----------------------------------------------------------------------------------------------
+#Функция приветствия
+def choice():
+    print("Отлично, друг!")
+    print("Я умею:")
+    print("\t[1] - выведу список файлов текущей директория")
+    print("\t[2] - выведу информацию об операционной системе")
+    print("\t[3] - выведу список id процессов")
+    print("\t[4] - дублирую файлы в текущей директории")
+    print("\t[5] - дублирую указанный файл")
+    print("\t[6] - удаляю дибликаты")
+    do = int(input("Укажите номер действия: "))
+    return do
+#Функция проверки выполнения операции дублирования
+def duplikate_file(file_name):
+    #Проверка является ли элемент файлом
+    if os.path.isfile(file_name):
+        newfile = file_name + '.dupl'
+        #копируем
+        shutil.copy(file_name, newfile)
+        #Проверяем
+        if os.path.exists(newfile):
+            print("Файл ",newfile," создан")
+            return True
+        else:
+            print("Возникли проблемы коирования.")
+            return False
+#Функция вывода информации о системе
+def sys_info():
+    print("Вот, что я знаю о системе: ")
+    print("\tКоличество процессоров: ", psutil.cpu_count())
+    print("\tПлатформа: ", sys.platform)
+    print("\tКодировка файловой системы: ", sys.getfilesystemencoding())
+    print("\tТекущая директория: ", os.getcwd())
+    print("\tТекущий пользователь: ", os.getlogin())
+#Функция удалениябуликатов
+def delete():
+    
+#----------------------------------------------------------------------------------------------
 print("\t\tРобот помошник Python")
 print("\t\tПривет, программист!")
 name = input("Ваше имя: ")
@@ -29,43 +56,22 @@ while True:
     answer = input("Давайте поработаем? (Y/N) ")
          
     if answer == 'Y' or answer == 'y':
-        print("Отлично, друг!")
-        print("Я умею:")
-        print("\t[1] - выведу список файлов текущей директория")
-        print("\t[2] - выведу информацию об операционной системе")
-        print("\t[3] - выведу список id процессов")
-        print("\t[4] - дублирую файлы в текущей директории")
-        print("\t[5] - дублирую указанный файл")
-        print("\t[6] - удаляю дибликаты")
-        do = int(input("Укажите номер действия: "))
+        
+        do = choice()
         
         if do == 1:
             print(os.listdir())
         elif do == 2:
-            print("Вот, что я знаю о системе: ")
-            print("\tКоличество процессоров: ", psutil.cpu_count())
-            print("\tПлатформа: ", sys.platform)
-            print("\tКодировка файловой системы: ", sys.getfilesystemencoding())
-            print("\tТекущая директория: ", os.getcwd())
-            print("\tТекущий пользователь: ", os.getlogin())
+            sys_info()
         elif do == 3:
             print(psutil.pids())
         elif do == 4:
             file_list = os.listdir()
             for f in file_list:
-                #Проверка является ли элемент файлом
-                if os.path.isfile(f):
-                    newfile = f + '.dupl'
-                    #копируем
-                    shutil.copy(f, newfile)
+                duplikate_file(f) 
         elif do == 5:
             file_name = input("Введите имя файла и относительный путь до него (. - текущая): ")
-            #Проверка является ли элемент файлом
-            if os.path.isfile(file_name):
-                newfile = file_name + '.dupl'
-                #копируем
-                shutil.copy(file_name, newfile)
-                
+            duplikate_file(file_name)          
         elif do == 6:
             dirname = input("Укажите относительную директорию (. - текущая): ")# . текущая
             file_list = os.listdir(dirname)
@@ -78,10 +84,6 @@ while True:
                 
         else:
             print("Введите цифру из списка!")
-       
-    # Функция type выводит информацию о типе переменной (объекта)
-    # Функция dir показывает внутреннее содержимое переменной (объекта) - атрибуты, методы
-    # Функция help выводит встронную справку об объекте
         
     elif answer == 'N' or answer == 'n':    
         print("До свидания!")
